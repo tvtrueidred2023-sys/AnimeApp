4a3130228b648bb721b99524372c895b814dae80
tvtrueidred2023-sys <tvtrueidred2023@gmail.com>
2025-11-16 06:57:23 +0700

    Delete app/src/main/java/com/example/animeapp directory

--- app/src/main/java/com/example/animeapp/network/AnimeLoadListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.animeapp.network;
-
-public interface AnimeLoadListener {
-    void onProgress(int currentEpisode, int totalEpisodes);
-    void onTotalEpisodes(int totalEpisodes);
-}
